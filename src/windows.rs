89254797db39844e909fe297bb5adb7b91308a86
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-11-20 13:03:45 +0200

    windows error log

--- src/windows.rs
+++ src/windows.rs
@@ -36,6 +36,7 @@ impl ECMAScriptAtomicWaitImpl for Racy<'_, u32> {
             Ok(())
         } else {
             let errno = std::io::Error::last_os_error().raw_os_error().unwrap_or(0);
+            eprintln!("Errno: {errno:?}");
             if errno == ERROR_TIMEOUT as i32 {
                 Err(FutexError::Timeout)
             } else {
